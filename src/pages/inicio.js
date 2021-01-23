2af046c595aea63f94c83ffe801a0027a36ec99a
BrayanM03 <natsu5679@gmail.com>
2021-01-22 21:24:49 -0600

    home

---
+++ src/pages/inicio.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import Card from '../components/card'
+
+
+function Inicio() {
+    constructor(props){
+        super(props)
+        
+    }
+    
+
+    return(
+        <div className="contenedor">
+<Bienvenida
+    usuario="Brayan"
+/>
+{
+    this.state.data.map()
+}
+<Card
+titulo="Inicio"
+descripcion="Soy el inicio"
+gradiente="160deg, #0093E9 0%, #80D0C7 100%"
+/>
+</div>
+
+    )
+  }
+
+export default Incio
